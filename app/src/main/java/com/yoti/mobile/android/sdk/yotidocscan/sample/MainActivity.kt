1faaaa6b6326e1ebd92cb2bebd68eb9186aa2c90
Mircea Bucerzan <mircea.bucerzan@yoti.com>
2025-08-12 16:58:33 +0300

    Enable edge-to-edge and handle insets
    Use the material3 Scaffold to wrap the MainScreen, because
    it provides the insets as parameter ready for use.
    
    Related to YD-6939

--- app/src/main/java/com/yoti/mobile/android/sdk/yotidocscan/sample/MainActivity.kt
+++ app/src/main/java/com/yoti/mobile/android/sdk/yotidocscan/sample/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.yoti.mobile.android.sdk.yotidocscan.sample
 
+import android.graphics.Color.TRANSPARENT
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import com.yoti.mobile.android.sdk.yotidocscan.sample.ui.theme.YotiDocScanSampleAppTheme
 import com.yoti.mobile.android.yotisdkcore.YotiSdk
 import com.yoti.mobile.android.yotisdkcore.YotiSdkContract
@@ -18,6 +24,16 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge(
+                statusBarStyle = SystemBarStyle.light(
+                        scrim = TRANSPARENT,
+                        darkScrim = TRANSPARENT
+                ),
+                navigationBarStyle = SystemBarStyle.light(
+                        scrim = TRANSPARENT,
+                        darkScrim = TRANSPARENT
+                )
+        )
 
         yotiSdk = YotiSdk(this@MainActivity)
 
@@ -37,22 +53,25 @@ class MainActivity : ComponentActivity() {
                     showSessionStatus()
                 }
 
-                MainScreen(
-                        sessionId = sessionId,
-                        onSessionIdChanged = { sessionId = it },
-                        sessionToken = sessionToken,
-                        onSessionTokenChanged = { sessionToken = it },
-                        sessionStatus = sessionStatus,
-                        onScanDocumentClicked = {
-                            val success = yotiSdk
-                                    .setSessionId(sessionId)
-                                    .setClientSessionToken(sessionToken)
-                                    .start(launcher)
-                            if (!success) {
-                                showSessionStatus()
-                            }
-                        }
-                )
+                Scaffold { innerPadding ->
+                    MainScreen(
+                            sessionId = sessionId,
+                            onSessionIdChanged = { sessionId = it },
+                            sessionToken = sessionToken,
+                            onSessionTokenChanged = { sessionToken = it },
+                            sessionStatus = sessionStatus,
+                            onScanDocumentClicked = {
+                                val success = yotiSdk
+                                        .setSessionId(sessionId)
+                                        .setClientSessionToken(sessionToken)
+                                        .start(launcher)
+                                if (!success) {
+                                    showSessionStatus()
+                                }
+                            },
+                            modifier = Modifier.padding(innerPadding)
+                    )
+                }
             }
         }
     }
